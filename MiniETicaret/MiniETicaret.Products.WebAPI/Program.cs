a8372a0c6eda5ffa615a60d466a1b687967fc7bb
Serkan Karışan <serkankarisan1905@gmail.com>
2024-11-26 17:47:57 +0300

    Products microservis kısmında Program.cs ayarları yapıldı.

--- MiniETicaret/MiniETicaret.Products.WebAPI/Program.cs
+++ MiniETicaret/MiniETicaret.Products.WebAPI/Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MiniETicaret.Products.WebAPI.Context;
+using MiniETicaret.Products.WebAPI.Dtos;
+using MiniETicaret.Products.WebAPI.Models;
 
 namespace MiniETicaret.Products.WebAPI
 {
@@ -16,6 +18,37 @@ namespace MiniETicaret.Products.WebAPI
 
             app.MapGet("/", () => "Hello World!");
 
+            app.MapGet("/getall", async (ApplicationDbContext context, CancellationToken cancellationToken) =>
+            {
+                var products = await context.Products.OrderBy(q => q.Name).ToListAsync(cancellationToken);
+                return products;
+            });
+            app.MapPost("/create", async (CreateProductDto request, ApplicationDbContext context, CancellationToken cancellationToken) =>
+            {
+                bool isNameExists = await context.Products.AnyAsync(q => q.Name == request.Name, cancellationToken);
+                if (isNameExists)
+                {
+                    return "Ürün adı daha önce oluşturulmuş";
+                }
+
+                Product product = new Product
+                {
+                    Name = request.Name,
+                    Price = request.Price,
+                    Stock = request.Stock,
+                };
+                await context.Products.AddAsync(product, cancellationToken);
+                await context.SaveChangesAsync(cancellationToken);
+
+                return "Ürün kaydı başarıyla oluşturuldu.";
+            });
+
+            using (var scoped = app.Services.CreateScope())
+            {
+                var srv = scoped.ServiceProvider;
+                var context = srv.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
             app.Run();
         }
     }
